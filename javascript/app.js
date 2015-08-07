85327f6d8888ea4f1423c41df06e198b7d276054
daariff <aariff.deen@gmail.com>
2015-08-07 16:06:08 -0700

    Got angular to work yeee

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,20 @@
-var myApp = angular.module('myApp',[]); 
+angular.module('myApp', [])
 
-  	.controller('MainCtrl', ['$scope', 
-        function($scope) {
-            $scope.HeaderOne = "Projects";
-            
-    }]);
-
-
+	.controller('MainCtrl', function($scope) {
+		$scope.HeaderOne = "Projects";
+		$scope.HeaderTwo = "Designs";
+		$scope.HeaderThree = "Contact Me";
+	    $scope.buttons = [
+	        {name:'Intro'},
+	        {name:'Projects'},
+	        {name:'Design'},
+	        {name:'Contact'},
+	        {name:'Login'},
+	    ];
+	    $scope.links = [
+	        {name:'LinkedIn', link: "LinkedIn.com"},
+	        {name:'GitHub', link: "Github.com/deenaariff"},
+	        {name:'Youtube', link: "youtube.com"},
+	        {name:'Facebook', link: "https://facebook.com/deen.aariff"},
+	    ];
+});
